23d4d1063ff1c5a114e078a81c65414eca8b99ad
abel <abelcheruvathoor@gmail.com>
2020-06-04 02:11:22 +0530

    Added firebase connections and contact list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:contacts_service/contacts_service.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Contact> contacts = [];
+  List<Contact> contactsFiltered = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getAllContacts();
+  }
+
+  getAllContacts() async {
+    List<Contact> _contacts = (await ContactsService.getContacts()).toList();
+    setState(() {
+      contacts = _contacts;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PicsCall',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to PicsCall'),
+          title: Text("PicsCall"),
         ),
-        body: Center(
-          child: Text('Hi from MLH'),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: contacts.length,
+                    itemBuilder: (context, index) {
+                      Contact contact = contacts[index];
+                      return ListTile(
+                        title: Text(contact.displayName),
+                        subtitle: Text(contact.phones.elementAt(0).value),
+                        leading: (contact.avatar != null &&
+                                contact.avatar.length > 0)
+                            ? CircleAvatar(
+                                backgroundImage: MemoryImage(contact.avatar),
+                              )
+                            : CircleAvatar(
+                                child: Text(contact.initials()),
+                              ),
+                      );
+                    }),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
